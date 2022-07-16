6503eb5f2b0a52387bf0e5d32a95e9ed2cb3695a
Alexander Molchanov <bonext@mailbox.org>
2022-07-16 21:21:11 +0300

    Change mouse shortuts for tags

--- config.def.h
+++ config.def.h
@@ -110,8 +110,8 @@ static Button buttons[] = {
 	{ ClkClientWin,         MODKEY,             Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY|ShiftMask,   Button1,        resizemouse,    {0} },
 	{ ClkTagBar,            0,                  Button1,        view,           {0} },
-	{ ClkTagBar,            0,                  Button3,        toggleview,     {0} },
+	{ ClkTagBar,            ShiftMask,          Button1,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,             Button1,        tag,            {0} },
-	{ ClkTagBar,            MODKEY,             Button3,        toggletag,      {0} },
+	{ ClkTagBar,            MODKEY|ShiftMask,   Button1,        toggletag,      {0} },
 };
 
